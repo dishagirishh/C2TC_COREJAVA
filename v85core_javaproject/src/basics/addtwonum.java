abb72ffbc514f493ca4464b0cfba46443b944714
Disha Girish <dishagirishh@gmail.com>
2024-03-24 18:55:54 +0530

    Second

---
+++ v85core_javaproject/src/basics/addtwonum.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class addtwonum {
+	public static void main(String args[]) {
+		short a=2;
+		short b=4;
+		int c=a+b;
+		System.out.println(c+" is the sum of two numbers");
+	}
+}
